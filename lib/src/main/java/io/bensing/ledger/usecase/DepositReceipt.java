5a7714cbf3cc3bf1c3622ab1bce7881f65f637de
Bill Bensing <10779425+BillBensing@users.noreply.github.com>
2022-12-13 20:08:48 -0500

    Created DepositReceipt and tests

--- lib/src/main/java/io/bensing/ledger/usecase/DepositReceipt.java
+++ lib/src/main/java/io/bensing/ledger/usecase/DepositReceipt.java
@@ -1,39 +1,81 @@
 package io.bensing.ledger.usecase;
 
-public class DepositReceipt {
+import io.bensing.kernel.Outcome;
+import io.bensing.kernel.SuccessfulOutcome;
+import io.bensing.kernel.UnsuccessfulOutcome;
 
-    public DepositReceipt() {
+public class DepositReceipt implements Outcome {
 
+    private long transactionId = 0L;
+    private long datAndTime = 0L;
+    private long userId = 0L;
+    private double money = 00.00;
+    private String currency = "";
+    private final Outcome outcome;
+
+    /**
+     * Create an instance of a successful deposit receipt.
+     *
+     * @param transactionId The Transaction ID of the deposit transaction
+     * @param dateAndTime The Date and Time of the deposit transaction
+     * @param userId The User ID the transaction was for
+     * @param money The amount of money the transaction was for
+     * @param currency The currency of the money for the transaction
+     */
+    public DepositReceipt(long transactionId, long dateAndTime, long userId, double money, String currency) {
+        this.transactionId = transactionId;
+        this.datAndTime = dateAndTime;
+        this.userId = userId;
+        this.money = money;
+        this.currency = currency;
+        this.outcome = new SuccessfulOutcome();
+    }
+
+    /***
+     * Create an instance of an unsuccessful deposit receipt.
+     *
+     * @param transactionId The Transaction ID of the deposit transaction
+     * @param dateAndTime The Date and Time of the deposit transaction
+     * @param userId The User ID the transaction was for
+     * @param money The amount of money the transaction was for
+     * @param currency The currency of the money for the transaction
+     */
+    public DepositReceipt(long transactionId, long dateAndTime, long userId, double money, String currency, String errorMessage) {
+        this.transactionId = transactionId;
+        this.datAndTime = dateAndTime;
+        this.userId = userId;
+        this.money = money;
+        this.currency = currency;
+        this.outcome = new UnsuccessfulOutcome(errorMessage);
     }
 
     public long TransactionId() {
-        return 0;
+        return this.transactionId;
     }
 
     public long DateAndTime() {
-        return 0;
+        return this.datAndTime;
     }
 
     public long UserId() {
-        return 0;
+        return this.userId;
     }
 
     public double MoneyDeposited() {
-        return 0;
+        return this.money;
     }
 
     public String CurrencyDeposited() {
-        return "";
+        return this.currency;
     }
 
     public boolean WasSuccessful() {
-        return false;
+        return this.outcome.WasSuccessful();
     }
     public boolean HasError() {
-        return false;
+        return this.outcome.HasError();
     }
-
     public String ErrorMessage() {
-        return "Implement Error Message";
+        return this.outcome.ErrorMessage();
     }
 }
---
+++ lib/src/test/java/io/bensing/ledger/usecase/DepositReceipt_Tests.java
@@ -0,0 +1,59 @@
+package io.bensing.ledger.usecase;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class DepositReceipt_Tests {
+
+    @Test
+    @Tag("small")
+    @DisplayName("Create a successful deposit receipt.")
+    public void CreateSuccessful_DepositReceipt(){
+
+        long transId = 2L;
+        long dateTime = 1234123412;
+        long userId = 383938393;
+        double money = 25.00;
+        String currency = "USD";
+
+        var receipt = new DepositReceipt(transId, dateTime, userId, money, currency);
+
+        Assertions.assertEquals(transId, 2L);
+        Assertions.assertEquals(dateTime, 1234123412);
+        Assertions.assertEquals(userId, 383938393);
+        Assertions.assertEquals(money, 25.00);
+        Assertions.assertEquals(currency, "USD");
+
+        Assertions.assertTrue(receipt.WasSuccessful());
+        Assertions.assertFalse(receipt.HasError());
+        Assertions.assertEquals("", receipt.ErrorMessage());
+    }
+
+    @Test
+    @Tag("small")
+    @DisplayName("Create a unsuccessful deposit receipt.")
+    public void CreateUnsuccessful_DepositReceipt(){
+
+        long transId = 2L;
+        long dateTime = 1234123412;
+        long userId = 383938393;
+        double money = 25.00;
+        String currency = "USD";
+        String errorMessage = "Ledger is full, keep your cash.";
+
+        var receipt = new DepositReceipt(transId, dateTime, userId, money, currency, errorMessage);
+
+        Assertions.assertEquals(transId, 2L);
+        Assertions.assertEquals(dateTime, 1234123412);
+        Assertions.assertEquals(userId, 383938393);
+        Assertions.assertEquals(money, 25.00);
+        Assertions.assertEquals(currency, "USD");
+
+        Assertions.assertFalse(receipt.WasSuccessful());
+        Assertions.assertTrue(receipt.HasError());
+        Assertions.assertEquals("Ledger is full, keep your cash.", receipt.ErrorMessage());
+    }
+
+}
