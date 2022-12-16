7f557bde63ab1caa7902ecb8926a98a8980cc41f
Bill Bensing <10779425+BillBensing@users.noreply.github.com>
2022-12-15 20:22:50 -0500

    added notes for the new suqence for make deposit

--- lib/src/main/java/io/bensing/ledger/usecase/MakeDeposit.java
+++ lib/src/main/java/io/bensing/ledger/usecase/MakeDeposit.java
@@ -16,6 +16,13 @@ public class MakeDeposit implements Outcome {
     }
 
     public DepositReceipt Deposit(long userId, double money, String currency) {
+        // TODO - Create the debit and credit transactions
+        // Sequence - Get the "CASH" credit account number          Create ChartOfAccountsGateway
+        // Sequence - Get the "Wallet" account number for user
+        // Create the credit for [account]                          create a Credit value object
+        // Create the debit for [account]                           create a Debit value object
+        // Update the chart of accounts with the debit & credit     Create UpdateLedgerGateway - refactor deposit gateway
+
         var gatewayResponse = this.depositGateway.Deposit(userId, money, currency);
         this.outcome = new SuccessfulOutcome();
         return new DepositReceipt(
