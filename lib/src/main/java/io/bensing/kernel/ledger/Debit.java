73a6f0c8b724c5fdf111e1b88476a250227a0969
Bill Bensing <10779425+BillBensing@users.noreply.github.com>
2022-12-15 20:50:57 -0500

    Added the debit value object

--- lib/src/main/java/io/bensing/kernel/ledger/Debit.java
+++ lib/src/main/java/io/bensing/kernel/ledger/Debit.java
@@ -1,2 +1,38 @@
-package io.bensing.kernel.ledger;public class Debit {
+package io.bensing.kernel.ledger;
+
+public class Debit {
+
+    private String description;
+    private long transactionId;
+    private long accountNumber;
+    private double amount;
+    private long dateAndTime;
+
+    public Debit(String description, long transactionId, long accountNumber, double amount, long dateAndTime) {
+        this.description = description;
+        this.transactionId = transactionId;
+        this.accountNumber = accountNumber;
+        this.amount = amount;
+        this.dateAndTime = dateAndTime;
+    }
+
+    public String Description() {
+        return this.description;
+    }
+
+    public long TransactionId() {
+        return this.transactionId;
+    }
+
+    public long AccountNumber() {
+        return this.accountNumber;
+    }
+
+    public double Amount() {
+        return this.amount;
+    }
+
+    public long DateAndTime() {
+        return this.dateAndTime;
+    }
 }
