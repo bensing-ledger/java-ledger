59dfe22f590bec58bcf559f4f5e76ec36455155a
Bill Bensing <10779425+BillBensing@users.noreply.github.com>
2022-12-16 13:36:57 -0600

    Added account kernel value object

--- src/main/java/io/bensing/ledger/kernel/Account.java
+++ src/main/java/io/bensing/ledger/kernel/Account.java
@@ -1,9 +1,10 @@
 package io.bensing.ledger.kernel;
 
 import io.bensing.kernel.identity.Id;
+import io.bensing.kernel.interfaces.Comparable;
 import io.bensing.kernel.interfaces.ValueObject;
 
-public class Account implements ValueObject<Long> {
+public class Account implements ValueObject<Long>, Comparable<Account> {
     private Id accountNumber;
 
     public Account(long accountNumber) {
@@ -12,6 +13,14 @@ public class Account implements ValueObject<Long> {
 
     public Long Value() {
         return this.accountNumber.Value();
+    }
+    public Id Number() {
+        return this.accountNumber;
+    }
+
+    @Override
+    public boolean Equals(Account compareAccount) {
+        return this.accountNumber.Equals(compareAccount.Number());
     }
 }
 
--- src/test/java/io/bensing/ledger/kernel/AccountTests.java
+++ src/test/java/io/bensing/ledger/kernel/AccountTests.java
@@ -1,5 +1,6 @@
 package io.bensing.ledger.kernel;
 
+import io.bensing.kernel.identity.Id;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -15,5 +16,28 @@ public class AccountTests {
         var account = new Account(1L);
 
         Assertions.assertEquals(1L, account.Value());
+        Assertions.assertTrue(new Id(1L).Equals(account.Number()));
+    }
+
+    @Test
+    @Tag("Small")
+    @DisplayName("Accounts with the same Id are equal")
+    public void AccountsAreEqual() {
+
+        var account1 = new Account(1L);
+        var account2 = new Account(1L);
+
+        Assertions.assertTrue(account1.Equals(account2));
+    }
+
+    @Test
+    @Tag("Small")
+    @DisplayName("Accounts with the different Id are not equal")
+    public void AccountsAreNotEqual() {
+
+        var account1 = new Account(1L);
+        var account2 = new Account(2L);
+
+        Assertions.assertFalse(account1.Equals(account2));
     }
 }
