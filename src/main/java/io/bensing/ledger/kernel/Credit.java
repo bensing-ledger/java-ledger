1c365f4244556dc6df4cf9a6ff8e7725e6dcda04
Bill Bensing <10779425+BillBensing@users.noreply.github.com>
2022-12-20 13:56:53 -0500

    Removed all the features from Credit except account and amount.  Journal Entry is taking over these other responsibilities

--- src/main/java/io/bensing/ledger/kernel/Credit.java
+++ src/main/java/io/bensing/ledger/kernel/Credit.java
@@ -1,80 +1,33 @@
 package io.bensing.ledger.kernel;
 
-import io.bensing.kernel.Validation;
-import io.bensing.kernel.identity.Id;
 import io.bensing.kernel.interfaces.Comparable;
-import io.bensing.kernel.interfaces.Validatable;
-import io.bensing.kernel.values.Content;
 
-import java.util.ArrayList;
+import java.util.Objects;
 
-public class Credit implements Comparable<Credit>, Validatable {
+public class Credit implements Comparable<Credit> {
 
-    private final Content description;
-    private final Id transactionId;
     private final Account account;
-    private final double amount;
-    private final long dateAndTime;
-    private Validation validation;
+    private final Amount amount;
 
-    public Credit(String description, Id transactionId, Account account, double amount, long dateAndTime) {
-        this.description = new Content(description);
-        this.transactionId = transactionId;     // TODO - Add Validation - Credit - transactionId
+    // TODO - Update the Credit and Debit to tak in the Account and Amount.
+    // TODO - Credit - Add an Account Type (Liability, Asset, ect..) to the account object.  The debit and credit will use this info to sign, or unsign, the amount.
+    public Credit(Account account, Amount amount) {
         this.account = account;
-        this.amount = amount;                   // TODO - Add Validation - Credit - amount
-        this.dateAndTime = dateAndTime;         // TODO - Add Validation - Credit - dateAndTime
-        this.validate();
+        this.amount = amount;
     }
 
-    public String Description() {
-        return this.description.Value();
+    public Long Account() {
+        return this.account.Value();
     }
-    public Id TransactionId() {
-        return this.transactionId;
-    }
-    public Account Account() {
-        return this.account;
-    }
-    public double Amount() {
-        return this.amount;
-    }
-    public long DateAndTime() {
-        return this.dateAndTime;
+    public Double Amount() {
+        return this.amount.Value();
     }
 
-    public boolean Equals(Credit compareCredit) {
-        var desc = this.Description().equals(compareCredit.Description());
-        var transId = this.TransactionId().Equals(compareCredit.TransactionId());
-        var acct = this.Account().Equals(compareCredit.Account());
-        var amt = this.Amount() == compareCredit.Amount();
-        var dateTime = this.DateAndTime() == compareCredit.DateAndTime();
-        return desc && transId && acct && amt && dateTime;
-    }
-    public boolean IsValid() {
-        return this.validation.IsValid();
-    }
-    public boolean IsInvalid() {
-        return this.validation.IsInvalid();
-    }
-    public ArrayList<String> ValidationMessages() {
-        return this.validation.ValidationMessages();
-    }
 
-    private void validate() {
-        this.validation = new Validation();
-        validateAccount(this.validation);
-        validateDescription(this.validation);
-    }
-
-    private void validateDescription(Validation validation) {
-        if (this.description.Value().length() != 0){ return; }
-        validation.AddMessage("Please provide a credit description.");
-    }
-
-    private void validateAccount(Validation validation) {
-        if (this.account.IsValid()) { return; }
-        var message = "The account number is invalid.";
-        validation.AddMessage(message);
-        validation.IncludeMessagesFrom(this.account.ValidationMessages());
+    @Override
+    public boolean Equals(Credit credit) {
+        var sameAccount = Objects.equals(credit.Account(), this.Account());
+        var sameAmount = Objects.equals(credit.Amount(), this.Amount());
+        return sameAccount && sameAmount;
     }
 }
--- src/test/java/io/bensing/ledger/kernel/CreditTests.java
+++ src/test/java/io/bensing/ledger/kernel/CreditTests.java
@@ -1,6 +1,5 @@
 package io.bensing.ledger.kernel;
 
-import io.bensing.kernel.identity.Id;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -12,19 +11,15 @@ public class CreditTests {
     @Tag("Small")
     @DisplayName("Create a Credit")
     public void CreateCredit() {
-        String description = "Transfer into account 1234";
-        var transactionId = new Id(1234234534);
+
         var account = new Account(1234);
-        double amount = 25.00;
-        long dateAndTime = 223423453;
+        var amount = new Amount(25.00);
 
-        var credit = new Credit(description, transactionId, account, amount, dateAndTime);
+        var credit = new Credit(account, amount);
 
-        Assertions.assertEquals("Transfer into account 1234", credit.Description());
-        Assertions.assertEquals(1234234534, credit.TransactionId().Value());
-        Assertions.assertEquals(1234, credit.Account().Value());
+        Assertions.assertEquals(1234, credit.Account());
         Assertions.assertEquals(25.00, credit.Amount());
-        Assertions.assertEquals(223423453, credit.DateAndTime());
+
     }
 
     @Test
@@ -32,10 +27,8 @@ public class CreditTests {
     @DisplayName("Two same credits equal each other.")
     public void TwoCreditsEqual() {
 
-        var credit1 = new Credit("Transfer into account 1234",
-                new Id(1234234534), new Account(1234), 25.00, 1L);
-        var credit2 = new Credit("Transfer into account 1234",
-                new Id(1234234534), new Account(1234), 25.00, 1L);
+        var credit1 = new Credit(new Account(1234), new Amount(25.00));
+        var credit2 = new Credit(new Account(1234), new Amount(25.00));
 
         Assertions.assertTrue(credit1.Equals(credit2));
 
@@ -46,58 +39,17 @@ public class CreditTests {
     @DisplayName("Two different credits do not equal each other.")
     public void TwoDifferentCreditsDoNotEqual() {
 
-        var credit1 = new Credit("Message 1",
-                new Id(1L), new Account(1234), 25.00, 1L);
-        var credit2 = new Credit("Message 2",
-                new Id(1L), new Account(1234), 25.00, 1L);
-
-        Assertions.assertFalse(credit1.Equals(credit2), "The two different credits descriptions should not equate");
-
-        credit1 = new Credit("Transfer into account 1234",
-                new Id(1L), new Account(1234), 25.00, 1L);
-        credit2 = new Credit("Transfer into account 1234",
-                new Id(2L), new Account(1234), 25.00, 1L);
-
-        Assertions.assertFalse(credit1.Equals(credit2), "The two different credits transaction ids should not equate");
-
-        credit1 = new Credit("Transfer into account 1234",
-                new Id(1L), new Account(3L), 25.00, 1L);
-        credit2 = new Credit("Transfer into account 1234",
-                new Id(1L), new Account(4L), 25.00, 1L);
+        var credit1 = new Credit(new Account(3L), new Amount(25.00));
+        var credit2 = new Credit(new Account(4L), new Amount(25.00));
 
         Assertions.assertFalse(credit1.Equals(credit2), "The two different credit account numbers should not equate");
 
-        credit1 = new Credit("Transfer into account 1234",
-                new Id(1L), new Account(3L), 30.00, 1L);
-        credit2 = new Credit("Transfer into account 1234",
-                new Id(1L), new Account(3L), 25.00, 1L);
+        credit1 = new Credit(new Account(3L), new Amount(35.00));
+        credit2 = new Credit(new Account(3L), new Amount(25.00));
 
         Assertions.assertFalse(credit1.Equals(credit2), "The two different credit amounts should not equate");
 
-        credit1 = new Credit("Transfer into account 1234",
-                new Id(1L), new Account(3L), 25.00, 5L);
-        credit2 = new Credit("Transfer into account 1234",
-                new Id(1L), new Account(3L), 25.00, 6L);
-
-        Assertions.assertFalse(credit1.Equals(credit2), "The two different credit date & times should not equate");
-
     }
 
-    @Test
-    @Tag("Small")
-    @DisplayName("Create an invalid Credit with an bad Account")
-    public void CreateInvalidCredit_FromInvalidAccount() {
 
-        String description = "Transfer into account 1234";
-        var transactionId = new Id(1234234534);
-        var account = new Account(0L);
-        double amount = 25.00;
-        long dateAndTime = 223423453;
-
-        var credit = new Credit(description, transactionId, account, amount, dateAndTime);
-
-        Assertions.assertFalse(credit.IsValid(), "Expected the credit to not be valid.");
-        Assertions.assertTrue(credit.ValidationMessages().contains("The account number is invalid."),
-                "The expected an invalid account number message was not provided. \n" + credit.ValidationMessages().toString());
-    }
 }
