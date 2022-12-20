8140995512510c68a9f944146fcf7dd7bd3e3b16
Bill Bensing <10779425+BillBensing@users.noreply.github.com>
2022-12-20 14:04:41 -0500

    Removed all features except amount and account, will be adding those to Journal Entry

--- src/main/java/io/bensing/ledger/kernel/Debit.java
+++ src/main/java/io/bensing/ledger/kernel/Debit.java
@@ -2,50 +2,29 @@ package io.bensing.ledger.kernel;
 
 import io.bensing.kernel.interfaces.Comparable;
 
+import java.util.Objects;
+
 public class Debit implements Comparable<Debit> {
 
-    private final JournalEntryDescription description;
-    private final JournalEntryId journalEntryId;
     private final Account account;
-    private final double amount;
-    private final long dateAndTime;
+    private final Amount amount;
 
-    // TODO - add a validation object to this to validate the incoming arguments are valid.
-
-    public Debit(JournalEntryDescription description, JournalEntryId journalEntryId, Account account, double amount, long dateAndTime) {
-        this.description = description;
-        this.journalEntryId = journalEntryId;
+    public Debit(Account account, Amount amount) {
         this.account = account;
         this.amount = amount;
-        this.dateAndTime = dateAndTime;
     }
 
-    public JournalEntryDescription Description() {
-        return this.description;
-    }
-
-    public JournalEntryId EntryId() {
-        return this.journalEntryId;
-    }
-
-    public Account Account() {
-        return this.account;
+    public long Account() {
+        return this.account.Value();
     }
 
     public double Amount() {
-        return this.amount;
+        return this.amount.Value();
     }
 
-    public long DateAndTime() {
-        return this.dateAndTime;
-    }
-
-    public boolean Equals(Debit compareDebit) {
-        var desc = this.Description().Equals(compareDebit.Description());
-        var transId = this.EntryId().Equals(compareDebit.EntryId());
-        var acct = this.Account().Equals(compareDebit.Account());
-        var amt = this.Amount() == compareDebit.Amount();
-        var dateTime = this.DateAndTime() == compareDebit.DateAndTime();
-        return desc && transId && acct && amt && dateTime;
+    public boolean Equals(Debit debit) {
+        var sameAccount = Objects.equals(debit.Account(), this.Account());
+        var sameAmount = Objects.equals(debit.Amount(), this.Amount());
+        return sameAccount && sameAmount;
     }
 }
--- src/test/java/io/bensing/ledger/kernel/DebitTests.java
+++ src/test/java/io/bensing/ledger/kernel/DebitTests.java
@@ -12,19 +12,14 @@ public class DebitTests {
     @Tag("Small")
     @DisplayName("Create a Debit")
     public void CreateDebit() {
-        var description = new JournalEntryDescription("Cash deposit into account 1234");
-        var transactionId = new JournalEntryId(1234234534);
         var account = new Account(1234);
-        double amount = 25.00;
-        long dateAndTime = 223423453;
+        var amount = new Amount(25.00);
 
-        var debit = new Debit(description, transactionId, account, amount, dateAndTime);
+        var debit = new Debit(account, amount);
 
-        Assertions.assertEquals("Cash deposit into account 1234", debit.Description().Value());
-        Assertions.assertEquals(1234234534, debit.EntryId().Value());
-        Assertions.assertEquals(1234, debit.Account().Value());
+        Assertions.assertEquals(1234, debit.Account());
         Assertions.assertEquals(25.00, debit.Amount());
-        Assertions.assertEquals(223423453, debit.DateAndTime());
+
     }
 
     @Test
@@ -32,10 +27,8 @@ public class DebitTests {
     @DisplayName("Two same debits equal each other.")
     public void TwoDebitsEqual() {
 
-        var debit1 = new Debit(new JournalEntryDescription("Transfer into account 1234"),
-                new JournalEntryId(1234234534), new Account(1234), 25.00, 1L);
-        var debit2 = new Debit(new JournalEntryDescription("Transfer into account 1234"),
-                new JournalEntryId(1234234534), new Account(1234), 25.00, 1L);
+        var debit1 = new Debit(new Account(1234), new Amount(25.00));
+        var debit2 = new Debit(new Account(1234), new Amount(25.00));
 
         Assertions.assertTrue(debit1.Equals(debit2));
 
@@ -46,40 +39,15 @@ public class DebitTests {
     @DisplayName("Two different debits do not equal each other.")
     public void TwoDifferentDebitsDoNotEqual() {
 
-        var debit1 = new Debit(new JournalEntryDescription("Message 1"),
-                new JournalEntryId(1L), new Account(1234), 25.00, 1L);
-        var debit2 = new Debit(new JournalEntryDescription("Message 2"),
-                new JournalEntryId(1L), new Account(1234), 25.00, 1L);
-
-        Assertions.assertFalse(debit1.Equals(debit2), "The two different debits descriptions should not equate");
-
-        debit1 = new Debit(new JournalEntryDescription("Transfer into account 1234"),
-                new JournalEntryId(1L), new Account(1234), 25.00, 1L);
-        debit2 = new Debit(new JournalEntryDescription("Transfer into account 1234"),
-                new JournalEntryId(2L), new Account(1234), 25.00, 1L);
-
-        Assertions.assertFalse(debit1.Equals(debit2), "The two different debits transaction ids should not equate");
-
-        debit2 = new Debit(new JournalEntryDescription("Transfer into account 1234"),
-                new JournalEntryId(1L), new Account(4L), 25.00, 1L);
-        debit1 = new Debit(new JournalEntryDescription("Transfer into account 1234"),
-                new JournalEntryId(1L), new Account(3L), 25.00, 1L);
+        var debit2 = new Debit(new Account(4L), new Amount(25.00));
+        var debit1 = new Debit(new Account(3L), new Amount(25.00));
 
         Assertions.assertFalse(debit1.Equals(debit2), "The two different debit account numbers should not equate");
 
-        debit1 = new Debit(new JournalEntryDescription("Transfer into account 1234"),
-                new JournalEntryId(1L), new Account(3L), 30.00, 1L);
-        debit2 = new Debit(new JournalEntryDescription("Transfer into account 1234"),
-                new JournalEntryId(1L), new Account(3L), 25.00, 1L);
+        debit1 = new Debit(new Account(3L), new Amount(30.00));
+        debit2 = new Debit(new Account(3L), new Amount(25.00));
 
         Assertions.assertFalse(debit1.Equals(debit2), "The two different debit amounts should not equate");
-
-        debit1 = new Debit(new JournalEntryDescription("Transfer into account 1234"),
-                new JournalEntryId(1L), new Account(3L), 25.00, 5L);
-        debit2 = new Debit(new JournalEntryDescription("Transfer into account 1234"),
-                new JournalEntryId(1L), new Account(3L), 25.00, 6L);
-
-        Assertions.assertFalse(debit1.Equals(debit2), "The two different debit date & times should not equate");
 
     }
 }
