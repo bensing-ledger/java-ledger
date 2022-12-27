c310bf546f98bae967785a42f501e9705d4a0780
Bill Bensing <10779425+BillBensing@users.noreply.github.com>
2022-12-27 10:07:57 -0500

    Added SmallTest implementation

--- src/test/java/io/bensing/ledger/jupiter/SmallTest.java
+++ src/test/java/io/bensing/ledger/jupiter/SmallTest.java
@@ -1,4 +1,17 @@
-package io.bensing.test.junit.jupiter;
+package io.bensing.ledger.jupiter;
 
-public class SmallTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("small")
+@Test
+public @interface SmallTest {
 }
