0ffc39696a207cf9e18bfc43a15a4a4af5ab4c91
Bill Bensing <10779425+BillBensing@users.noreply.github.com>
2022-12-27 15:48:27 -0500

    Updated JournalEntry to include the new Journal entry objects and validations

--- src/main/java/io/bensing/ledger/kernel/JournalEntry.java
+++ src/main/java/io/bensing/ledger/kernel/JournalEntry.java
@@ -1,41 +1,103 @@
 package io.bensing.ledger.kernel;
 
+import io.bensing.kernel.Validation;
 import io.bensing.kernel.identity.Id;
-import io.bensing.kernel.interfaces.Comparable;
-import io.bensing.kernel.interfaces.ValueObject;
+import io.bensing.kernel.interfaces.Validatable;
 
-public class JournalEntry {
+import java.util.ArrayList;
 
-    private final String description;
-    private final Credit credit;
-    private final Debit debit;
-    private Id transactionId;
+public class JournalEntry implements Validatable {
 
-    // TODO - add a validation object to this to validate the incoming arguments are valid.
-    public JournalEntry(String description, Id transId, long dateAndTime, double amount, Account creditAcct, Account debitAcct) {
-        this.description = description;
-        this.transactionId = transId;
-        this.credit = new Credit(this.description, this.transactionId, creditAcct, amount, dateAndTime);
-        this.debit = new Debit(this.description, this.transactionId, debitAcct, amount, dateAndTime);
+    private final JournalEntryDescription description;
+    private final JournalEntryId journalEntryId;
+    private final JournalEntryDate journalEntryDate;
+
+    private final Account creditAccount;
+    private final Account debitAccount;
+    private final Amount amount;
+
+    private Validation validation;
+
+    public JournalEntry(String description, Id entryId, long dateAndTime, double amount, Account creditAcct, Account debitAcct) {
+        this.description = new JournalEntryDescription(description);
+        this.journalEntryId = new JournalEntryId(entryId);
+        this.amount = new Amount(amount);
+        this.journalEntryDate = new JournalEntryDate(dateAndTime);
+        this.creditAccount = creditAcct;
+        this.debitAccount = debitAcct;
+        this.validate();
     }
 
-    // TODO - add a validation to the Credt & Debit
-    public boolean IsValid() {
-        return true;
-    }
-
-    public String Description() {
+    public JournalEntryDescription Description() {
         return this.description;
     }
-
-    public Id TransactionId() {
-        return this.transactionId;
+    public JournalEntryId EntryId() {
+        return this.journalEntryId;
     }
-
+    public JournalEntryDate Date() { return this.journalEntryDate; }
     public Credit Credit() {
-        return this.credit;
+        if (this.IsInvalid()) { return new Credit(this.creditAccount,new Amount(0)); }
+        return new Credit(this.creditAccount, this.amount);
     }
     public Debit Debit() {
-        return this.debit;
+        if (this.IsInvalid()) { return new Debit(this.debitAccount,new Amount(0)); }
+        return new Debit(this.debitAccount, this.amount);
+    }
+
+    public boolean IsValid() {
+        return this.validation.IsValid();
+    }
+    public boolean IsInvalid() {
+        return this.validation.IsInvalid();
+    }
+    public ArrayList<String> ValidationMessages() {
+        return this.validation.ValidationMessages();
+    }
+
+    private void validate() {
+        this.validation = new Validation();
+        validateDescription(this.validation);
+        validateId(this.validation);
+        validateAmount(this.validation);
+        validateDate(this.validation);
+        validateCreditAccount(this.validation);
+        validateDebitAccount(this.validation);
+    }
+    private void validateDescription(Validation validation) {
+        if (this.description.IsValid()){ return; }
+        var message = "The journal entry description is invalid.";
+        validation.AddMessage(message);
+        validation.IncludeMessagesFrom(this.description.ValidationMessages());
+    }
+    private void validateId(Validation validation) {
+        if (this.journalEntryId.IsValid() ) { return; }
+        var message = "The journal entry id is invalid.";
+        validation.AddMessage(message);
+        validation.IncludeMessagesFrom(this.journalEntryId.ValidationMessages());
+    }
+    private void validateAmount(Validation validation) {
+        if (this.amount.IsValid()) { return; }
+        var message = "The journal entry amount is invalid.";
+        validation.AddMessage(message);
+        validation.IncludeMessagesFrom(this.amount.ValidationMessages());
+
+    }
+    private void validateDate(Validation validation) {
+        if (this.journalEntryDate.IsValid()) { return; }
+        var message = "The journal entry date is invalid.";
+        validation.AddMessage(message);
+        validation.IncludeMessagesFrom(this.journalEntryDate.ValidationMessages());
+    }
+    private void validateCreditAccount(Validation validation) {
+        if (this.creditAccount.IsValid()) { return; }
+        var message = "The journal credit account is invalid.";
+        validation.AddMessage(message);
+        validation.IncludeMessagesFrom(this.creditAccount.ValidationMessages());
+    }
+    private void validateDebitAccount(Validation validation) {
+        if (this.debitAccount.IsValid()) { return; }
+        var message = "The journal debit account is invalid.";
+        validation.AddMessage(message);
+        validation.IncludeMessagesFrom(this.debitAccount.ValidationMessages());
     }
 }
--- src/test/java/io/bensing/ledger/kernel/JournalEntryTests.java
+++ src/test/java/io/bensing/ledger/kernel/JournalEntryTests.java
@@ -1,41 +1,223 @@
 package io.bensing.ledger.kernel;
 
 import io.bensing.kernel.identity.Id;
+import io.bensing.ledger.jupiter.SmallTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 
-import java.time.Instant;
-
+@DisplayName("Journal Entry - Positive & Negative")
 public class JournalEntryTests {
 
-    @Test
-    @Tag("small")
-    @DisplayName("Create a Journal Entry")
+    @SmallTest
+    @DisplayName("Create a Journal Entry, move $20 from one asset account to another.")
     public void CreateJournalEntry(){
 
-        double amount = 20.00;
-        var transId = new Id(1L);
+        var description = "New Transaction";
+        var entryId = new Id(1L);
         var dateAndTime = 22L;
-        var creditAcct = new Account(21L);
-        var debitAcct = new Account(31L);
+        double amount = 20.00;
+        var creditAcct = new Account(21L, AccountType.Asset);
+        var debitAcct = new Account(31L, AccountType.Asset);
 
-        var journalEntry = new JournalEntry("New Transaction", transId, dateAndTime, amount, creditAcct, debitAcct);
+        var entry = new JournalEntry(description, entryId, dateAndTime, amount, creditAcct, debitAcct);
 
-        var expectedCredit = new Credit("New Transaction", transId, creditAcct, amount, dateAndTime);
-        var expectedDebit = new Debit("New Transaction", transId, debitAcct, amount, dateAndTime);
+        Assertions.assertTrue(entry.IsValid(), "Expected IsValid() to be true.");
+        Assertions.assertFalse(entry.IsInvalid(), "Expected IsInvalid() to be false.");
+        Assertions.assertEquals(0, entry.ValidationMessages().size(),
+                "Expected validation messages to have no (0) messages.");
 
-        Assertions.assertTrue(journalEntry.IsValid(),
-                "The journal entry is supposed to be valid, and it is not valid.");
-        Assertions.assertEquals("New Transaction", journalEntry.Description(),
+        Assertions.assertEquals("New Transaction", entry.Description().Value(),
                 "The journal entry transaction description did not match the expected transaction description");
-        Assertions.assertEquals(1L, journalEntry.TransactionId().Value(), "" +
+        Assertions.assertEquals(1L, entry.EntryId().Value(), "" +
                 "The journal entry transaction id did not match the expected transaction id.");
-        Assertions.assertTrue(journalEntry.Credit().Equals(expectedCredit),
-                "The journal entry credit did not match the expected credit.");
-        Assertions.assertTrue(journalEntry.Debit().Equals(expectedDebit),
-                "The journal entry debit did not match the expected debit.");
+        Assertions.assertEquals(22L, entry.Date().Value(),
+                "The journal entry date did not match the expected date.");
+
+        Assertions.assertTrue(entry.Credit().IsValid(), "Expected the credit to be valid, but it is Invalid.");
+        Assertions.assertEquals(-20.00, entry.Credit().Value(), "The expected credit value was not returned.");
+        Assertions.assertEquals(21L, entry.Credit().AccountNumber(),
+                "The journal entry credit account number did not match the expected account number.");
+
+        Assertions.assertTrue(entry.Debit().IsValid(), "Expected the debit to be valid, but it is Invalid.");
+        Assertions.assertEquals(20.00, entry.Debit().Value(), "The expected debit value was not returned.");
+        Assertions.assertEquals(31L, entry.Debit().AccountNumber(),
+                "The journal entry debit account number did not match the expected account number.");
 
     }
+
+    @SmallTest
+    @DisplayName("Create an invalid journal entry with an bad description")
+    public void MakeInvalid_FromInvalidDescription() {
+
+        var invalidDescription = "";
+        var entryId = new Id(1234234534);
+        var creditAccount = new Account(1L, AccountType.Asset);
+        var debitAccount = new Account(1L, AccountType.Asset);
+        var amount = 25.00;
+        long dateAndTime = 223423453;
+
+        var entry = new JournalEntry(invalidDescription, entryId, dateAndTime, amount, creditAccount, debitAccount);
+
+        Assertions.assertTrue(entry.IsInvalid(), "Expected IsInvalid() to be true.");
+        Assertions.assertFalse(entry.IsValid(), "Expected IsValid() to be false.");
+        Assertions.assertTrue(entry.ValidationMessages().contains("The journal entry description is invalid."),
+                "The expected invalid journal entry description message was not provided.");
+
+        Assertions.assertTrue(entry.Credit().IsInvalid(), "Expected the credit to be invalid, but it is valid.");
+        Assertions.assertEquals(0, entry.Credit().Value(), "Expected the credit value to be zero (0).");
+        Assertions.assertTrue(entry.Debit().IsInvalid(), "Expected the debit to be invalid, but it is valid.");
+        Assertions.assertEquals(0, entry.Debit().Value(), "Expected the debit value to be zero (0).");
+    }
+
+    @SmallTest
+    @DisplayName("Create an invalid journal entry with a bad entry Id.")
+    public void MakeInvalid_FromInvalidEntryId() {
+
+        var description = "A good description";
+        var badEntryId = new Id(0L);
+        var creditAccount = new Account(1L, AccountType.Asset);
+        var debitAccount = new Account(2L, AccountType.Asset);
+        var amount = 25.00;
+        long dateAndTime = 223423453;
+
+        var entry = new JournalEntry(description, badEntryId, dateAndTime, amount, creditAccount, debitAccount);
+
+        Assertions.assertTrue(entry.IsInvalid(), "Expected IsInvalid() to be true.");
+        Assertions.assertFalse(entry.IsValid(), "Expected IsValid() to be false.");
+        Assertions.assertTrue(entry.ValidationMessages().contains("The journal entry id is invalid."),
+                "The expected invalid journal entry id message was not provided.");
+
+        Assertions.assertTrue(entry.Credit().IsInvalid(), "Expected the credit to be invalid, but it is valid.");
+        Assertions.assertEquals(0, entry.Credit().Value(), "Expected the credit value to be zero (0).");
+        Assertions.assertTrue(entry.Debit().IsInvalid(), "Expected the debit to be invalid, but it is valid.");
+        Assertions.assertEquals(0, entry.Debit().Value(), "Expected the debit value to be zero (0).");
+    }
+
+    @SmallTest
+    @DisplayName("Create an invalid journal entry with a negative amount.")
+    public void MakeInvalid_FromNegativeAmount() {
+
+        var description = "A good description";
+        var badAmount = -10.00;
+        var entryId = new Id(1L);
+        var creditAccount = new Account(1L, AccountType.Asset);
+        var debitAccount = new Account(2L, AccountType.Asset);
+
+        long dateAndTime = 223423453;
+
+        var entry = new JournalEntry(description, entryId, dateAndTime, badAmount, creditAccount, debitAccount);
+
+        Assertions.assertTrue(entry.IsInvalid(), "Expected IsInvalid() to be true.");
+        Assertions.assertFalse(entry.IsValid(), "Expected IsValid() to be false.");
+        Assertions.assertTrue(entry.ValidationMessages().contains("The journal entry amount is invalid."),
+                "The expected invalid journal entry amount message was not provided.");
+
+        Assertions.assertTrue(entry.Credit().IsInvalid(), "Expected the credit to be invalid, but it is valid.");
+        Assertions.assertEquals(0, entry.Credit().Value(), "Expected the credit value to be zero (0).");
+        Assertions.assertTrue(entry.Debit().IsInvalid(), "Expected the debit to be invalid, but it is valid.");
+        Assertions.assertEquals(0, entry.Debit().Value(), "Expected the debit value to be zero (0).");
+    }
+
+    @SmallTest
+    @DisplayName("Create an invalid journal entry with a zero amount.")
+    public void MakeInvalid_FromZeroAmount() {
+
+        var description = "A good description";
+        var badAmount = 00.00;
+        var entryId = new Id(1L);
+        var creditAccount = new Account(1L, AccountType.Asset);
+        var debitAccount = new Account(2L, AccountType.Asset);
+
+        long dateAndTime = 223423453;
+
+        var entry = new JournalEntry(description, entryId, dateAndTime, badAmount, creditAccount, debitAccount);
+
+        Assertions.assertTrue(entry.IsInvalid(), "Expected IsInvalid() to be true.");
+        Assertions.assertFalse(entry.IsValid(), "Expected IsValid() to be false.");
+        Assertions.assertTrue(entry.ValidationMessages().contains("The journal entry amount is invalid."),
+                "The expected invalid journal entry amount message was not provided.");
+
+        Assertions.assertTrue(entry.Credit().IsInvalid(), "Expected the credit to be invalid, but it is valid.");
+        Assertions.assertEquals(0, entry.Credit().Value(), "Expected the credit value to be zero (0).");
+        Assertions.assertTrue(entry.Debit().IsInvalid(), "Expected the debit to be invalid, but it is valid.");
+        Assertions.assertEquals(0, entry.Debit().Value(), "Expected the debit value to be zero (0).");
+    }
+
+    @SmallTest
+    @DisplayName("Create an invalid journal entry with a bad date.")
+    public void MakeInvalid_FromZeroDate() {
+
+        var description = "A good description";
+        var amount = 25.00;
+        var entryId = new Id(1L);
+        var creditAccount = new Account(1L, AccountType.Asset);
+        var debitAccount = new Account(2L, AccountType.Asset);
+
+        long badDate = 0L;
+
+        var entry = new JournalEntry(description, entryId, badDate, amount, creditAccount, debitAccount);
+
+        Assertions.assertTrue(entry.IsInvalid(), "Expected IsInvalid() to be true.");
+        Assertions.assertFalse(entry.IsValid(), "Expected IsValid() to be false.");
+        Assertions.assertTrue(entry.ValidationMessages().contains("The journal entry date is invalid."),
+                "The expected invalid journal entry date message was not provided.");
+
+        Assertions.assertTrue(entry.Credit().IsInvalid(), "Expected the credit to be invalid, but it is valid.");
+        Assertions.assertEquals(0, entry.Credit().Value(), "Expected the credit value to be zero (0).");
+        Assertions.assertTrue(entry.Debit().IsInvalid(), "Expected the debit to be invalid, but it is valid.");
+        Assertions.assertEquals(0, entry.Debit().Value(), "Expected the debit value to be zero (0).");
+    }
+
+    @SmallTest
+    @DisplayName("Create an invalid journal entry with a bad credit account.")
+    public void MakeInvalid_FromBadCreditAccount() {
+
+        var description = "A good description";
+        var amount = 00.00;
+        var entryId = new Id(1L);
+        var badCreditAccount = new Account(0L, AccountType.Asset);
+        var debitAccount = new Account(2L, AccountType.Asset);
+
+        long dateAndTime = 223423453;
+
+        var entry = new JournalEntry(description, entryId, dateAndTime, amount, badCreditAccount, debitAccount);
+
+        Assertions.assertTrue(entry.IsInvalid(), "Expected IsInvalid() to be true.");
+        Assertions.assertFalse(entry.IsValid(), "Expected IsValid() to be false.");
+        var expectedMessage = "The journal credit account is invalid.";
+        Assertions.assertTrue(entry.ValidationMessages().contains(expectedMessage),
+                "The expected invalid journal credit account message was not provided.");
+
+        Assertions.assertTrue(entry.Credit().IsInvalid(), "Expected the credit to be invalid, but it is valid.");
+        Assertions.assertEquals(0, entry.Credit().Value(), "Expected the credit value to be zero (0).");
+        Assertions.assertTrue(entry.Debit().IsInvalid(), "Expected the debit to be invalid, but it is valid.");
+        Assertions.assertEquals(0, entry.Debit().Value(), "Expected the debit value to be zero (0).");
+    }
+
+    @SmallTest
+    @DisplayName("Create an invalid journal entry with a bad debit account.")
+    public void MakeInvalid_FromBadDebitAccount() {
+
+        var description = "A good description";
+        var amount = 00.00;
+        var entryId = new Id(1L);
+        var creditAccount = new Account(1L, AccountType.Asset);
+        var badDebitAccount = new Account(0L, AccountType.Asset);
+
+        long dateAndTime = 223423453;
+
+        var entry = new JournalEntry(description, entryId, dateAndTime, amount, creditAccount, badDebitAccount);
+
+        Assertions.assertTrue(entry.IsInvalid(), "Expected IsInvalid() to be true.");
+        Assertions.assertFalse(entry.IsValid(), "Expected IsValid() to be false.");
+        var expectedMessage = "The journal debit account is invalid.";
+        Assertions.assertTrue(entry.ValidationMessages().contains(expectedMessage),
+                "The expected invalid journal debit account message was not provided.");
+
+        Assertions.assertTrue(entry.Credit().IsInvalid(), "Expected the credit to be invalid, but it is valid.");
+        Assertions.assertEquals(0, entry.Credit().Value(), "Expected the credit value to be zero (0).");
+        Assertions.assertTrue(entry.Debit().IsInvalid(), "Expected the debit to be invalid, but it is valid.");
+        Assertions.assertEquals(0, entry.Debit().Value(), "Expected the debit value to be zero (0).");
+    }
+
 }
