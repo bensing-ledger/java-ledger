843ec97200b42c4904a9042c476ad7cae357b5d6
Bill Bensing <10779425+BillBensing@users.noreply.github.com>
2022-12-27 15:05:34 -0500

    Added Validatable and ValueObject interface to Debit, added logic for amount based upon AccountType

--- src/main/java/io/bensing/ledger/kernel/Debit.java
+++ src/main/java/io/bensing/ledger/kernel/Debit.java
@@ -1,28 +1,71 @@
 package io.bensing.ledger.kernel;
 
+import io.bensing.kernel.Validation;
 import io.bensing.kernel.interfaces.Comparable;
+import io.bensing.kernel.interfaces.Validatable;
+import io.bensing.kernel.interfaces.ValueObject;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
-public class Debit implements Comparable<Debit> {
+public class Debit implements ValueObject<Double>, Comparable<Debit>, Validatable {
 
     private final Account account;
     private final Amount amount;
-    // TODO - Add the logic to determine if the amount returned is signed/unsigned based upon the account type (need new object AccountType[Asset, Liability, ect..]
+
+    private double value = 0;
+
+    private Validation validation;
+
     public Debit(Account account, Amount amount) {
         this.account = account;
         this.amount = amount;
+        this.validate();
+        this.determineValue();
     }
 
-    public Account Account() {
-        return this.account;
-    }
-    public Amount Amount() {
-        return this.amount;
-    }
+    public long AccountNumber() { return this.account.Value(); }
+    public Double Value() { return this.value; }
     public boolean Equals(Debit debit) {
-        var sameAccount = this.account.Equals(debit.Account());
-        var sameAmount = this.amount.Equals(debit.Amount());
+        var sameAccount = this.AccountNumber() == debit.AccountNumber();
+        var sameAmount = Objects.equals(this.Value(), debit.Value());
         return sameAccount && sameAmount;
     }
+    public boolean IsValid() {
+        return this.validation.IsValid();
+    }
+    public boolean IsInvalid() {
+        return this.validation.IsInvalid();
+    }
+    public ArrayList<String> ValidationMessages() {
+        return this.validation.ValidationMessages();
+    }
+
+    private void validate() {
+        this.validation = new Validation();
+        this.validateAccount(this.validation);
+        this.validateAmount(this.validation);
+    }
+    private void validateAccount(Validation validation) {
+        if (this.account.IsValid()) { return; }
+        this.validation.AddMessage("The account is invalid.");
+        this.validation.IncludeMessagesFrom(this.account.ValidationMessages());
+    }
+    private void validateAmount(Validation validation) {
+        if (amount.IsValid()) { return; }
+        this.validation.AddMessage("The amount is invalid.");
+        this.validation.IncludeMessagesFrom(amount.ValidationMessages());
+    }
+    private void determineValue() {
+        if (this.IsInvalid()) { return; }
+        if (this.account.Type().Equals(AccountType.Liability) ||
+                this.account.Type().Equals(AccountType.Revenue) ||
+                this.account.Type().Equals(AccountType.Equity)) {
+            this.value = -1 * this.amount.Value();
+            return;
+        }
+        this.value = this.amount.Value();
+    }
+
+
 }
--- src/test/java/io/bensing/ledger/kernel/DebitTests.java
+++ src/test/java/io/bensing/ledger/kernel/DebitTests.java
@@ -1,15 +1,13 @@
 package io.bensing.ledger.kernel;
 
+import io.bensing.ledger.jupiter.SmallTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 
 @DisplayName("Debit - Positive & Negative")
 public class DebitTests {
 
-    @Test
-    @Tag("Small")
+    @SmallTest
     @DisplayName("Create a Debit")
     public void CreateDebit() {
         var account = new Account(1234, AccountType.Asset);
@@ -17,15 +15,128 @@ public class DebitTests {
 
         var debit = new Debit(account, amount);
 
-        var expectedAccount = new Account(1234, AccountType.Asset);
-        var expectedAmount  = new Amount(25.00);
-        Assertions.assertTrue(expectedAccount.Equals(debit.Account()), "The expected account was NOT EQUAL to the debit Account");
-        Assertions.assertTrue(expectedAmount.Equals(debit.Amount()), "The expected amount was NOT EQUAL to the debit Amount.");
+        Assertions.assertEquals(1234, debit.AccountNumber(), "The expected account was NOT EQUAL to the debit Account");
+        Assertions.assertEquals(25.00, debit.Value(), "The expected value was NOT EQUAL to the debit value.");
+        Assertions.assertTrue(debit.IsValid(), "Expected IsValid() to be true.");
+        Assertions.assertFalse(debit.IsInvalid(), "Expected IsValid() to be false.");
 
     }
 
-    @Test
-    @Tag("Small")
+    @SmallTest
+    @DisplayName("Create the debit for a asset account type.")
+    public void CreateAssetDebit() {
+        var account = new Account(1234, AccountType.Asset);
+        var amount = new Amount(25.00);
+
+        var debit = new Debit(account, amount);
+
+        Assertions.assertEquals(1234, debit.AccountNumber(), "The expected account was NOT EQUAL to the debit Account");
+        Assertions.assertEquals(25.00, debit.Value(), "The expected value was NOT EQUAL to the debit value.");
+        Assertions.assertTrue(debit.IsValid(), "Expected IsValid() to be true.");
+        Assertions.assertFalse(debit.IsInvalid(), "Expected IsValid() to be false.");
+    }
+
+    @SmallTest
+    @DisplayName("Create the debit for a liability  type.")
+    public void CreateLiabilityDebit() {
+        var account = new Account(1234, AccountType.Liability);
+        var amount = new Amount(25.00);
+
+        var debit = new Debit(account, amount);
+
+        Assertions.assertEquals(1234, debit.AccountNumber(), "The expected account was NOT EQUAL to the debit Account");
+        Assertions.assertEquals(-25.00, debit.Value(), "The expected value was NOT EQUAL to the debit value.");
+        Assertions.assertTrue(debit.IsValid(), "Expected IsValid() to be true.");
+        Assertions.assertFalse(debit.IsInvalid(), "Expected IsValid() to be false.");
+    }
+
+    @SmallTest
+    @DisplayName("Create the debit for a revenue type.")
+    public void CreateRevenueDebit() {
+        var account = new Account(1234, AccountType.Revenue);
+        var amount = new Amount(25.00);
+
+        var debit = new Debit(account, amount);
+
+        Assertions.assertEquals(1234, debit.AccountNumber(), "The expected account was NOT EQUAL to the debit Account");
+        Assertions.assertEquals(-25.00, debit.Value(), "The expected value was NOT EQUAL to the debit value.");
+        Assertions.assertTrue(debit.IsValid(), "Expected IsValid() to be true.");
+        Assertions.assertFalse(debit.IsInvalid(), "Expected IsValid() to be false.");
+    }
+
+    @SmallTest
+    @DisplayName("Create the debit for a expense type.")
+    public void CreateExpenseDebit() {
+        var account = new Account(1234, AccountType.Expense);
+        var amount = new Amount(25.00);
+
+        var debit = new Debit(account, amount);
+
+        Assertions.assertEquals(1234, debit.AccountNumber(), "The expected account was NOT EQUAL to the debit Account");
+        Assertions.assertEquals(25.00, debit.Value(), "The expected value was NOT EQUAL to the debit value.");
+        Assertions.assertTrue(debit.IsValid(), "Expected IsValid() to be true.");
+        Assertions.assertFalse(debit.IsInvalid(), "Expected IsValid() to be false.");
+    }
+
+    @SmallTest
+    @DisplayName("Create the debit for a equity type.")
+    public void CreateEquityDebit() {
+        var account = new Account(1234, AccountType.Equity);
+        var amount = new Amount(25.00);
+
+        var debit = new Debit(account, amount);
+
+        Assertions.assertEquals(1234, debit.AccountNumber(), "The expected account was NOT EQUAL to the debit Account");
+        Assertions.assertEquals(-25.00, debit.Value(), "The expected value was NOT EQUAL to the debit value.");
+        Assertions.assertTrue(debit.IsValid(), "Expected IsValid() to be true.");
+        Assertions.assertFalse(debit.IsInvalid(), "Expected IsValid() to be false.");
+    }
+
+    @SmallTest
+    @DisplayName("Create an invalid debit from an invalid amount.")
+    public void CreateInvalidDebitFromInvalidAmount() {
+
+        var account = new Account(1234, AccountType.Asset);
+        var amount = new Amount(-25.00);
+
+        var debit = new Debit(account, amount);
+
+        Assertions.assertFalse(debit.IsValid(), "Expected IsValid() to be false.");
+        Assertions.assertTrue(debit.IsInvalid(), "Expected IsInvalid() to be true.");
+        var expectedMessage = "The amount is invalid.";
+        Assertions.assertTrue(debit.ValidationMessages().contains(expectedMessage),
+                "The expected invalid debit amount message was not provided.");
+        Assertions.assertTrue(debit.ValidationMessages().size() >= 2,
+                "Expected at least two validation messages.");
+
+        Assertions.assertEquals(00.00, debit.Value(), "The debit value was not the expected value.");
+        Assertions.assertEquals(1234, debit.AccountNumber(), "The debit account was not the expected account.");
+
+    }
+
+    @SmallTest
+    @DisplayName("Create an invalid debit from an invalid account.")
+    public void CreateInvalidDebitFromInvalidAccount() {
+
+        var account = new Account(0L, AccountType.Asset);
+        var amount = new Amount(25.00);
+
+        var debit = new Debit(account, amount);
+
+        Assertions.assertFalse(debit.IsValid(), "Expected IsValid() to be false.");
+        Assertions.assertTrue(debit.IsInvalid(), "Expected IsInvalid() to be true.");
+        var expectedMessage = "The account is invalid.";
+        Assertions.assertTrue(debit.ValidationMessages().contains(expectedMessage),
+                "The expected invalid debit account message was not provided.");
+        Assertions.assertTrue(debit.ValidationMessages().size() >= 2,
+                "Expected at least two validation messages.");
+
+        Assertions.assertEquals(00.00, debit.Value(), "The debit value was not the expected value.");
+        Assertions.assertEquals(0, debit.AccountNumber(), "The debit account was not the expected account.");
+
+    }
+
+    @SmallTest
     @DisplayName("Two same debits equal each other.")
     public void TwoDebitsEqual() {
 
@@ -36,8 +147,7 @@ public class DebitTests {
 
     }
 
-    @Test
-    @Tag("Small")
+    @SmallTest
     @DisplayName("Two different debits do not equal each other.")
     public void TwoDifferentDebitsDoNotEqual() {
 
